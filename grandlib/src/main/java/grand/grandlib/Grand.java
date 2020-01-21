5a284be15cabe017a81eba6d8724306fa71397b3
Mahmoud Ayman <mahmoud_aymann@outlook.com>
2020-01-21 14:50:43 +0200

    last commit

--- grandlib/src/main/java/grand/grandlib/Grand.java
+++ grandlib/src/main/java/grand/grandlib/Grand.java
@@ -6,7 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import grand.grandlib.constantsutils.Codes;
 
 public abstract class Grand {
-    //
+    /////
     public static void showDialog(AppCompatActivity activity){
         MovementManager.startDialogActivity(activity, Codes.GRAND_DIALOG, new Bundle());
     }
--- grandlib/src/main/java/grand/grandlib/GrandDialogFragment.java
+++ grandlib/src/main/java/grand/grandlib/GrandDialogFragment.java
@@ -4,54 +4,37 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 import androidx.fragment.app.Fragment;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-
 import net.grand.R;
-
+import net.grand.R2;
 import org.jetbrains.annotations.NotNull;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class GrandDialogFragment extends BaseFragment implements View.OnClickListener {
-
+public class GrandDialogFragment extends BaseFragment {
     public GrandDialogFragment() {
         // Required empty public constructor
     }
-
     @Override
     public View onCreateView(@NotNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_grand_dialog, container, false);
-
-        TextView textViewWhatsapp = view.findViewById(R.id.tv_whatsapp);
-        textViewWhatsapp.setOnClickListener(this);
-        TextView textViewCall = view.findViewById(R.id.tv_call);
-        textViewCall.setOnClickListener(this);
-        View view1 = view.findViewById(R.id.view1);
-        view1.setOnClickListener(this);
-        View view2 = view.findViewById(R.id.view2);
-        view2.setOnClickListener(this);
-        FloatingActionButton fab = view.findViewById(R.id.btn_close);
-        fab.setOnClickListener(this);
-
+        ButterKnife.bind(this, view);
         return view;
     }
-
-
-    @Override
-    public void onClick(View v) {
-        int id = v.getId();
-        if (id == R.id.tv_whatsapp) {
-            SettingsManager.whatsAppMsg(requireActivity(), getString(R.string.grand_phone));
-        }else if (id == R.id.tv_call){
+    @OnClick({R2.id.btn_call, R2.id.btn_whatsapp, R2.id.view1, R2.id.view2, R2.id.btn_close})
+    public void views(View view) {
+        if (view.getId() == R.id.btn_call) {
             SettingsManager.makeCall(requireActivity(), getString(R.string.grand_phone));
-        }else if (id == R.id.view1 || id == R.id.view2 || id == R.id.btn_close){
+        } else if (view.getId() == R.id.btn_whatsapp) {
+            SettingsManager.whatsAppMsg(requireActivity(), getString(R.string.grand_phone));
+        } else {
             requireActivity().onBackPressed();
         }
     }
+
 }
